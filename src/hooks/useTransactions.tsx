c3ffe1715699f2b07a4af8c0ec50c7bff33a671e
Adriano Moreira <adr_0603@yahoo.com.br>
2022-01-10 23:36:24 -0300

    creating hook

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from "react";
-import { api } from "./services/api";
+import { createContext, useEffect, useState, ReactNode, useContext } from "react";
+import { api } from "../services/api";
 
 
 interface Transaction {
@@ -22,7 +22,7 @@ interface TransactionContextData {
     createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionContextData>(
+const TransactionsContext = createContext<TransactionContextData>(
     {} as TransactionContextData
 );
 
@@ -56,3 +56,8 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
     )
 }
  
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context;
+}
